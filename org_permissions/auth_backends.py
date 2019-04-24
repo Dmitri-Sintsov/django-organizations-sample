c8957e037f528888fdf6b9c17bbb525afb30ea78
Dmitri Sintsov <questpc256@gmail.com>
2019-04-24 17:14:06 +0300

    Added User organizations permission caching to OrganizationModelBackend.

--- org_permissions/auth_backends.py
+++ org_permissions/auth_backends.py
@@ -7,19 +7,25 @@ from .models import OrganizationPermission
 
 class OrganizationModelBackend(ModelBackend):
 
-    # Todo: add caching.
+    perm_cache_name = '_organization_perm_cache'
+
     def get_organization_permissions(self, user_obj, obj=None):
+        if not user_obj.is_active or user_obj.is_anonymous or obj is not None:
+            return set()
+        if hasattr(user_obj, self.perm_cache_name):
+            return getattr(user_obj, self.perm_cache_name)
         user_organizations = user_obj.organizations_organization.all()
         user_organizations_permissions = OrganizationPermission.objects.filter(
             organization__in=Subquery(user_organizations.values('pk'))
         )
-        return user_organizations_permissions.annotate(
+        perms = user_organizations_permissions.annotate(
             perm=Concat('permission__content_type__app_label', Value('.'), 'permission__codename')
         ).values_list('perm', flat=True)
+        setattr(user_obj, self.perm_cache_name, set(perms))
+        return perms
 
     def has_perm(self, user_obj, perm, obj=None):
-        has_org_perm = perm in self.get_organization_permissions(user_obj, obj)
-        if has_org_perm:
+        if perm in self.get_organization_permissions(user_obj, obj):
             return True
         else:
             return super().has_perm(user_obj, perm, obj)
